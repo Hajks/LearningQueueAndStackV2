fb4ac3ca2a2c72e9371af49f1bd4e6690dc0ee4a
Hajk <tunczorx@gmail.com>
2018-05-30 08:18:11 +0200

    Possibilites of working with generic collections

--- LearningQueueAndStackV2/Form1.cs
+++ LearningQueueAndStackV2/Form1.cs
@@ -15,6 +15,16 @@ namespace LearningQueueAndStackV2
         public Form1()
         {
             InitializeComponent();
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("pierwszy w rzędzie");
+            myStack.Push("drugi w rzędzie");
+            myStack.Push("trzeci w rzędzie");
+            myStack.Push("czwarty w rzędzie");
+
+            Queue<string> myQueue = new Queue<string>(myStack); // becuse list/queue/stack implement IEnumerable interface we can change them beetwen themself and we can use foreach on all of them.
+            List<string> myList = new List<string>(myQueue);
+            Stack<string> anotherStack = new Stack<string>(myList);
+            MessageBox.Show ("myQueue ma " + myQueue.Count + " elementy \n" + "myList ma " + myList.Count + " elementy \n" + "anotherStack ma " + anotherStack.Count + " elementy \n");
         }
     }
 }
